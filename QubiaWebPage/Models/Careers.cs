01640990e9f88aa2fb9bed74c2a2c03aad3530c0
steven mata <smata9903@gmail.com>
2025-12-07 09:39:31 -0600

    Se completa el modelo

--- QubiaWebPage/Models/Careers.cs
+++ QubiaWebPage/Models/Careers.cs
@@ -11,7 +11,33 @@ namespace QubiaWebPage.Models
         public string NombreCompleto { get; set; }
 
         [Required]
+        [RegularExpression(@"^\d{9}$")] //incluyendo ceros
+        public string Cedula { get; set; } 
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
         [RegularExpression(@"^\d{9,12}$")]
-        public string Cedula { get; set; }
+        public string Telefono { get; set; }
+
+        [Required]
+        public string Estudios { get; set; }
+
+        [Required]  
+        public  string ComoConocio { get; set; }
+
+        [Required]
+        public bool ActualmenteTrabaja { get; set; }
+
+        [Required]
+        [Range (450000,6000000)]
+        public decimal ExpectativaSalarial { get; set; }
+
+        [Required]
+        public string CV {  get; set; }
+
+
     }
 }
