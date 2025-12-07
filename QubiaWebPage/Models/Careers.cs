02ef84d42602d8e179da809cba70bbba9bfa3c26
steven mata <smata9903@gmail.com>
2025-12-07 09:28:33 -0600

    Se realiza modelo las variables ID, nombre completo y cedula

---
+++ QubiaWebPage/Models/Careers.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QubiaWebPage.Models
+{
+    public class Careers
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{9,12}$")]
+        public string Cedula { get; set; }
+    }
+}
